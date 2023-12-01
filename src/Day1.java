7a2c45d7b0ef15cb85b0bcd44e2fde001dd5b42c
Christian Leskowsky <christian@leskowsky.net>
2023-12-01 14:54:52 -0500

    Part 2 completed. Will try to clean this up somewhat ...

--- src/Day1.java
+++ src/Day1.java
@@ -1,42 +1,49 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day1 {
-    public static int[] digits(String s) {
-        return s.chars()
-                .filter(Character::isDigit)
-                .map(x -> Character.digit(x, 10))
-                .toArray();
+
+    public static boolean PART_B = true;
+
+    public static List<Integer> digits(String s) {
+        var digits = "0123456789";
+        var wordDigits = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+
+        var ret = new ArrayList<Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            var c = s.charAt(i);
+
+            if (digits.contains(String.valueOf(c))) {
+                ret.add(digits.indexOf(c));
+                continue;
+            }
+
+            if (PART_B) {
+                for (String d: wordDigits) {
+                    if (s.startsWith(d, i)) {
+                        ret.add(wordDigits.indexOf(d));
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ret;
     }
 
     public static int calibrationValue(String s) {
+        System.out.println(s);
         var digits = digits(s);
-        System.out.println(Arrays.toString(digits));
-        return digits[0] * 10 + digits[digits.length - 1];
-    }
-
-    public static String wordsToDigits(String s) {
-        var wordList = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
-        var x = s;
-        for (int i = 0; i < wordList.size(); i++) {
-            x = x.replaceAll(wordList.get(i), Integer.toString(i));
-        }
-        System.out.println(x);
-        return x;
+        System.out.println(digits);
+        return digits.get(0) * 10 + digits.get(digits.size() - 1);
     }
 
     public static void main(String[] args) throws Exception {
-//        var x = Files.readAllLines(Path.of("inputs/day1.txt")).stream()
-//                .map(Day1::calibrationValue)
-//                .reduce(Integer::sum);
-//        System.out.println(x);
-
-        var y = Files.readAllLines(Path.of("inputs/day1.txt")).stream()
-                .map(Day1::wordsToDigits)
+        var x = Files.readAllLines(Path.of("inputs/day1.txt")).stream()
                 .map(Day1::calibrationValue)
                 .reduce(Integer::sum);
-        System.out.println(y);
+        System.out.println(x);
     }
 }
