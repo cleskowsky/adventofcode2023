b7cc8861fb1afde9b4ec5af65b40a1ea9b241c57
Christian Leskowsky <christian@leskowsky.net>
2023-12-01 10:03:23 -0500

    This isn't working for part 2 because numbers can overlap, and the order I'm scanning numbers in means a number that should appear first is consumed by the transform
    eg eightwo - my method would make this eigh2 meaning eight won't match
    
    Need to this of a different approach here (maybe firstNum and lastNum)
    
    Bit of subtlety to day 1 !

--- src/Day1.java
+++ src/Day1.java
@@ -1,5 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 public class Day1 {
     public static int[] digits(String s) {
@@ -11,13 +13,30 @@ public class Day1 {
 
     public static int calibrationValue(String s) {
         var digits = digits(s);
+        System.out.println(Arrays.toString(digits));
         return digits[0] * 10 + digits[digits.length - 1];
     }
 
+    public static String wordsToDigits(String s) {
+        var wordList = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+        var x = s;
+        for (int i = 0; i < wordList.size(); i++) {
+            x = x.replaceAll(wordList.get(i), Integer.toString(i));
+        }
+        System.out.println(x);
+        return x;
+    }
+
     public static void main(String[] args) throws Exception {
-        var x = Files.readAllLines(Path.of("inputs/day1.txt")).stream()
+//        var x = Files.readAllLines(Path.of("inputs/day1.txt")).stream()
+//                .map(Day1::calibrationValue)
+//                .reduce(Integer::sum);
+//        System.out.println(x);
+
+        var y = Files.readAllLines(Path.of("inputs/day1.txt")).stream()
+                .map(Day1::wordsToDigits)
                 .map(Day1::calibrationValue)
                 .reduce(Integer::sum);
-        System.out.println(x);
+        System.out.println(y);
     }
 }
