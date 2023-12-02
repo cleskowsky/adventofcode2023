f518686f61eade6dea137f02c6b39af5d84b12d8
Christian Leskowsky <christian@leskowsky.net>
2023-12-02 09:13:39 -0500

    Day 2 part 1 finished

--- src/Day2.java
+++ src/Day2.java
@@ -1,10 +1,31 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Day2 {
-    public static void main(String[] args) {
-        System.out.println(1);
+    public static void main(String[] args) throws Exception {
+        var in = Files.readAllLines(Path.of("inputs/day2.txt"));
+
+        var sum = 0;
+        for (String s : in) {
+            var x = parseGame(s);
+            if (gamesMatching(x)) {
+                System.out.println(x);
+                sum += x.id;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static boolean gamesMatching(Game g) {
+        for (Round r : g.rounds) {
+            if (r.red() > 12 || r.green() > 13 || r.blue() > 14) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static int parseGameID(String s) {
@@ -16,7 +37,8 @@ public class Day2 {
         for (String colour : s.split(", ")) {
             var split = colour.split(" ");
             ret.put(split[1], Integer.parseInt(split[0]));
-        };
+        }
+        ;
         return ret;
     }
 
@@ -49,7 +71,7 @@ public class Day2 {
         }
 
         private int getColour(String s) {
-            return get(s) != null ? get(s) : null;
+            return get(s) != null ? get(s) : 0;
         }
     }
 }
--- test/Day2Test.java
+++ test/Day2Test.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day2Test {
 
